d14fef3034b48ba20c238c89c16af8d3e1d15a6c
dayani <mkdkdesilva@gmail.com>
2025-12-16 20:34:15 +0530

    Create froget Password, Send OTP 🙌

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,24 @@ import axios from "axios";
 import User from "../models/user.js";
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+import OTP from "../models/otpModel.js";
+import getDesignEmail from "../lib/emailDesigner.js"
+
+
+dotenv.config();
+//transporter email geniyann hadanva
+const transporter = nodemailer.createTransport({//me okkoma default eva meva wenas karann ona na
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.APP_PASSWORD,
+    },
+});
 
 export function createUser(req, res) {
 
@@ -24,6 +42,7 @@ export function createUser(req, res) {
         }
     ).catch(
         () => {
+
             res.json({
                 message: "Fail to create user"
             })
@@ -47,6 +66,12 @@ export function loginUser(req, res) {
                 )
 
             } else {
+                if (user.isBlock) {
+                    res.status(403).json({
+                        message: "Your account has been blocked. Please contact admin.",
+                    });
+                    return;
+                }
                 const isPasswordMatching = bcrypt.compareSync(req.body.password, user.password)
 
                 if (isPasswordMatching) {
@@ -167,42 +192,50 @@ export async function googleLogin(req, res) {
         if (user == null) {//user kenek nathnm eyat aluthem account ekk hadann ona
 
             const newUser = new User({
-				email: googleUser.email,
-				firstName: googleUser.given_name,
-				lastName: googleUser.family_name,
-				password: "abc",
-				isEmailVerified: googleUser.email_verified,
-				image: googleUser.picture,
-			});
+                email: googleUser.email,
+                firstName: googleUser.given_name,
+                lastName: googleUser.family_name,
+                password: "abc",
+                isEmailVerified: googleUser.email_verified,
+                image: googleUser.picture,
+            });
 
             let savedUser = await newUser.save();
 
-			const jwtToken = jwt.sign(
-				{
-					email: savedUser.email,
-					firstName: savedUser.firstName,
-					lastName: savedUser.lastName,
-					role: savedUser.role,
-					isEmailVerified: savedUser.isEmailVerified,
-					image: savedUser.image,
-				},
-				process.env.JWT_SECRET
-			);
-			res.json({
-				message: "Login successful",
-				token: jwtToken,
-				user: {
-					email: savedUser.email,
-					firstName: savedUser.firstName,
-					lastName: savedUser.lastName,
-					role: savedUser.role,
-					isEmailVerified: savedUser.isEmailVerified,
-					image: savedUser.image,
-				},
-			});
-			return;
+            const jwtToken = jwt.sign(
+                {
+                    email: savedUser.email,
+                    firstName: savedUser.firstName,
+                    lastName: savedUser.lastName,
+                    role: savedUser.role,
+                    isEmailVerified: savedUser.isEmailVerified,
+                    image: savedUser.image,
+                },
+                process.env.JWT_SECRET
+            );
+            res.json({
+                message: "Login successful",
+                token: jwtToken,
+                user: {
+                    email: savedUser.email,
+                    firstName: savedUser.firstName,
+                    lastName: savedUser.lastName,
+                    role: savedUser.role,
+                    isEmailVerified: savedUser.isEmailVerified,
+                    image: savedUser.image,
+                },
+            });
+            return;
 
         } else {//login the user
+
+            if (user.isBlock) {
+                res.status(403).json({
+                    message: "Your account has been blocked. Please contact admin.",
+                });
+                return;
+            }
+
             const jwtToken = jwt.sign(
                 {
                     email: user.email,
@@ -215,18 +248,18 @@ export async function googleLogin(req, res) {
                 process.env.JWT_SECRET
             )
             res.json({
-				message: "Login successful",
-				token: jwtToken,
-				user: {
-					email: user.email,
-					firstName: user.firstName,
-					lastName: user.lastName,
-					role: user.role,
-					isEmailVerified: user.isEmailVerified,
-					image: user.image,
-				},
-			});
-			return;
+                message: "Login successful",
+                token: jwtToken,
+                user: {
+                    email: user.email,
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    role: user.role,
+                    isEmailVerified: user.isEmailVerified,
+                    image: user.image,
+                },
+            });
+            return;
 
         }
 
@@ -241,4 +274,210 @@ export async function googleLogin(req, res) {
 
 
 }
+
+export async function getAllUsers(req, res) {
+    if (!isAdmin(req)) {
+        res.status(403).json({
+            message: "Forbidden",
+        });
+        return;
+    }
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({
+            message: "Failed to get users",
+        });
+    }
+}
+
+export async function blockOrUnblockUser(req, res) {
+    console.log(req.user);
+    if (!isAdmin(req)) {
+        res.status(403).json({
+            message: "Forbidden",
+        });
+        return;
+    }
+
+    if (req.user.email === req.params.email) {
+        res.status(400).json({
+            message: "You cannot block yourself",
+        });
+        return;
+    }
+
+    try {
+        await User.updateOne(
+            {
+                email: req.params.email,
+            },
+            {
+                isBlock: req.body.isBlock,
+            }
+        );
+
+        res.json({
+            message: "User block status updated successfully",
+        });
+    } catch (err) {
+        res.status(500).json({
+            message: "Failed to block/unblock user",
+        });
+    }
+}
+
+//usert email ekt code ekk yavan vidiy froget password valadi
+
+export async function sendOTP(req, res) {
+    const email = req.params.email;//request eke parameter vala dala evan email ek gannva
+    if (email == null) {// email ekk nathnm
+        res.status(400).json({
+            message: "Email is required",
+        });
+        return;
+    }
+
+    // 100000 - 999999 number 6k random valu ekk me range ek atahra
+    const otp = Math.floor(100000 + Math.random() * 900000);
+
+    try {
+        const user = await User.findOne({ email: email });
+
+        const firstName = user ? user.firstName : "there";
+
+        if (user == null) {
+            res.status(404).json({
+                message: "User not found",
+            });
+            return;
+        }
+
+        await OTP.deleteMany({//email ekem thiyena okkoma OTP tika delete karanva
+            email: email,
+        });
+
+        const newOTP = new OTP({// new OTP 
+            email: email,
+            otp: otp.toString(),
+        });
+        await newOTP.save();// new OTP ek save kara gannva
+
+
+
+        await transporter.sendMail({//send karana mail ek
+            from: process.env.EMAIL_USER,
+            to: email,
+            subject: "Password Reset OTP – Crystal Beauty Clear",
+            html: getDesignEmail({ otp, userName: firstName }),
+
+        });
+
+        res.json({
+            message: "OTP sent to your email",
+        });
+
+        console.log("SEND OTP API HIT:", req.params.email);
+    } catch (err) {
+        console.error("SEND OTP ERROR FULL:", err);
+        res.status(500).json({
+            message: "Failed to send OTP",
+        });
+    }
+
+}
+//email ek verified karagann function ek
+export async function changePasswordViaOTP(req, res) {
+    const email = req.body.email;//mail ek
+    const otp = req.body.otp;//otp ek
+    const newPassword = req.body.newPassword;// new password ek
+    try {
+        const otpRecord = await OTP.findOne({//me email eke me otp ek thiyena ekk hoyagannva vena otp ahuvenne na
+            email: email,
+            otp: otp.toString().trim(),
+        });
+
+        if (otpRecord == null) {//OTP ekk nathnm message ekk yavanva invalid kiyla
+            res.status(400).json({
+                message: "Invalid OTP",
+            });
+            return;
+        }
+
+        await OTP.deleteMany({//me email ekt adala okkom otp tika delete karanva
+            email: email,
+        });
+
+        const hashedPassword = bcrypt.hashSync(newPassword, 10);//newpassword ek hash karanva
+
+        await User.updateOne(//ita passe userge email and password update kara gannva
+            {
+                email: email,
+            },
+            {
+                password: hashedPassword,
+            }
+        );
+        res.json({
+            message: "Password changed successfully",
+        });
+    } catch (err) {
+        res.status(500).json({
+            message: "Failed to change password",
+        });
+    }
+}
+
+// export async function updateUserData(req, res) {
+// 	if (req.user == null) {
+// 		res.status(401).json({
+// 			message: "Unauthorized",
+// 		});
+// 		return;
+// 	}
+
+// 	try{
+
+// 		await User.updateOne({
+// 			email: req.user.email
+// 		},{
+// 			firstName: req.body.firstName,
+// 			lastName: req.body.lastName,
+// 			image: req.body.image
+// 		})
+// 		res.json({
+// 			message: "User data updated successfully",
+// 		});
+// 	}catch(err){
+// 		res.status(500).json({
+// 			message: "Failed to update user data",
+// 		});
+// 	}
+// }
+
+// export async function updatePassword(req, res) {
+// 	if (req.user == null) {
+// 		res.status(401).json({
+// 			message: "Unauthorized",
+// 		});
+// 		return;
+// 	}
+// 	try{
+// 		const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+// 		await User.updateOne({
+// 			email: req.user.email
+// 		},{
+// 			password: hashedPassword
+// 		})
+// 		res.json({
+// 			message: "Password updated successfully",
+// 		});
+// 	}
+// 	catch(err){
+// 		res.status(500).json({
+// 			message: "Failed to update password",
+// 		});
+// 	}
+// }
 
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from "express" ;
+import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/userRouter.js";
 import jwt from "jsonwebtoken";
@@ -17,41 +17,43 @@ app.use(cors())
 app.use(express.json())
 //middleware authentication part
 app.use(
-    (req,res,next)=>{
+    (req, res, next) => {
         let token = req.header("Authorization") //token ek thiyenn ona request eke header eke Authorization eke kiyala dila tiyenne
-        if(token != null){ // token ekk thiyenvnm token ek print karanva
-            token = token.replace("Bearer ","")//Bearer  replace kara gannva mek nathuv token ek print karanna
+        
+
+        if (token != null) { // token ekk thiyenvnm token ek print karanva
+            token = token.replace("Bearer ", "")//Bearer  replace kara gannva mek nathuv token ek print karanna
             console.log(token)
 
             // token ek decrypt karanva
-            jwt.verify(token,process.env.JWT_SECRET,
+            jwt.verify(token, process.env.JWT_SECRET,
                 //decrypt vunath nathath me function ek run venva
-                (err, decoded)=>{
-                    if(decoded == null){
+                (err, decoded) => {
+                    if (decoded == null) {
                         res.json(
                             {
-                                message : "Invalid token please login again"
+                                message: "Invalid token please login again"
                             }
                         )
                         return // methanin ehata me function ek run venn epa 
-                    }else {
+                    } else {
                         req.user = decoded // request eka athulat userge details tikath dagannva, hebeyi thama mek katavth yevuve naa
                     }
-                    
-            })
+
+                })
         }
         next()//request ek adala kenat yavanva token ek weradi nathnm
-    }   
+    }
 )
 
 const connectionString = process.env.MONGO_URI
 
 mongoose.connect(connectionString).then(
-    ()=>{
+    () => {
         console.log("Database connected")
     }
 ).catch(
-     ()=>{
+    () => {
         console.log("Database connected failed")
     }
 )
@@ -62,6 +64,6 @@ app.use("/api/products", productRouter)
 app.use("/api/orders", orderRouter)
 
 
-app.listen(5000, (req,res)=>{ 
+app.listen(5000, (req, res) => {
     console.log("server is started")
 })
